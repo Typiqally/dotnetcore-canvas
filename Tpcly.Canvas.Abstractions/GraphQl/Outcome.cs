c304c4875f9f75683295c07416d542bc6897f7f6
Jelle Maas <typiqally@gmail.com>
2023-10-11 14:22:05 +0200

    Add mastery points to outcome

--- Tpcly.Canvas.Abstractions/GraphQl/Outcome.cs
+++ Tpcly.Canvas.Abstractions/GraphQl/Outcome.cs
@@ -4,5 +4,6 @@ namespace Tpcly.Canvas.Abstractions.GraphQl;
 
 public record Outcome(
     [property: JsonPropertyName("_id")] int Id,
-    [property: JsonPropertyName("title")] string Title
+    [property: JsonPropertyName("title")] string Title,
+    [property: JsonPropertyName("masteryPoints")] double? MasteryPoints
 );
