4f1e61ca963f21898daa72372c58895acbfdd933
Jelle Maas <typiqally@gmail.com>
2023-10-11 14:21:48 +0200

    Add HTML URL to assignment

--- Tpcly.Canvas.Abstractions/GraphQl/Assignment.cs
+++ Tpcly.Canvas.Abstractions/GraphQl/Assignment.cs
@@ -3,7 +3,8 @@ using System.Text.Json.Serialization;
 namespace Tpcly.Canvas.Abstractions.GraphQl;
 
 public record Assignment(
-    [property: JsonPropertyName("name")] string? Name, 
-    [property: JsonPropertyName("modules")] IEnumerable<Module>? Modules ,
-    [property: JsonPropertyName("rubric")] Rubric? Rubric 
+    [property: JsonPropertyName("name")] string? Name,
+    [property: JsonPropertyName("modules")] IEnumerable<Module>? Modules,
+    [property: JsonPropertyName("rubric")] Rubric? Rubric,
+    [property: JsonPropertyName("htmlUrl")] Uri? HtmlUrl
 );
