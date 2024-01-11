9481aa724276b95b00372b227a3217b1d0a2891d
Neal Geilen <info@nealgeilen.nl>
2024-01-12 00:19:41 +0100

    Add and change properties

--- Tpcly.Canvas.Abstractions/GraphQl/Course.cs
+++ Tpcly.Canvas.Abstractions/GraphQl/Course.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 namespace Tpcly.Canvas.Abstractions.GraphQl;
 
 public record Course(
+    [property: JsonPropertyName("_id")] string? Id,
     [property: JsonPropertyName("name")] string? Name,
     [property: JsonPropertyName("term")] EnrollmentTerm? Term,
     [property: JsonPropertyName("submissionsConnection")] GraphQlConnection<Submission>? Submissions,
