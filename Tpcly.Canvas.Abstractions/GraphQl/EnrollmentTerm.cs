bf8a62b5e4779995322c06f72886379f0230a791
Neal Geilen <info@nealgeilen.nl>
2024-01-12 12:21:49 +0100

    Add and change properties GraphQL (#13)
    * Add and change properties
    
    * Typo

--- Tpcly.Canvas.Abstractions/GraphQl/EnrollmentTerm.cs
+++ Tpcly.Canvas.Abstractions/GraphQl/EnrollmentTerm.cs
@@ -4,6 +4,7 @@ namespace Tpcly.Canvas.Abstractions.GraphQl;
 
 public record EnrollmentTerm(
     [property: JsonPropertyName("name")] string Name,
-    [property: JsonPropertyName("start_at")] DateTime? StartAt,
-    [property: JsonPropertyName("end_at")] DateTime? EndAt
+    [property: JsonPropertyName("_id")] string Id,
+    [property: JsonPropertyName("startAt")] DateTime? StartAt,
+    [property: JsonPropertyName("endAt")] DateTime? EndAt
 );
